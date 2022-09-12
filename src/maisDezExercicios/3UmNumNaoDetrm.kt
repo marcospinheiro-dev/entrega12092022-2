1d159b5deced66b386e152883ca5a73ff7ec2f46
marcospinheiro-dev <marcos.pinheiro.dev@gmail.com>
2022-09-12 16:31:44 -0300

    Branch1

--- src/maisDezExercicios/3UmNumNaoDetrm.kt
+++ src/maisDezExercicios/3UmNumNaoDetrm.kt
@@ -5,6 +5,7 @@ import java.util.*
 //média aritmética dos valores lidos, a quantidade de valores positivos, a quantidade de valores
 //negativos e o percentual de valores negativos e positivos.
 //Talvez eu não tenha entendido a questão como deveria ser.
+
 fun main() {
     val sc = Scanner(System.`in`)
     println("Informe um número inteiro positivo ou negativo: ")
